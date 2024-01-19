b970801ffeeeb70ee56e0b019bf17920af2a52be
ScarLLD <www.olegborisov1@gmail.com>
2024-01-19 13:45:59 +0300

    1

--- Assets/Skripts/Coin.cs
+++ Assets/Skripts/Coin.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    [SerializeField] private CoinCountDisplay _coinDisplayObject;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<Player>())
